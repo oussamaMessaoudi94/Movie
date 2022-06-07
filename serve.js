c8b0b92091b6b368632ce4714b340e9575b67b57
oussamaMessaoudi94 <oussamamessaoudi94@gmail.com>
2022-06-07 16:39:44 +0100

    Création du serveur avec Express

---
+++ serve.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/movie'));
+
+app.get('/*', function(req,res) {
+     res.sendFile(path.join(__dirname+'/dist/movie/index.html'));
+});
